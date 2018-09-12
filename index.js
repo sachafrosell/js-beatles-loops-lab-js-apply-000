8e69a6380bec4ed06a3e2188e2cf772e80e70934
Alexander Frosell <sachafrosell@gmail.com>
2018-09-12 20:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(arr1, arr2) {
   var emptyArr = [];
   for (let i = 0; i < arr1.length; i++) {
-    
+    emptyArr.push(`${arr1[i]} plays ${arr2[i]}`)
   }
+  return emptyArr;
 }
